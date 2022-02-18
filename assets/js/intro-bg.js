d6dbe2ff9b2d39018a0ff16eb17fc25c79db6791
David Rojo <rojo@rojorevolution.com>
2022-02-18 16:40:17 -0600

    bg slideshow now working

--- assets/js/intro-bg.js
+++ assets/js/intro-bg.js
@@ -1,10 +1,42 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM loaded')
+    let introBG = document.getElementById('intro-bg')
 
-    let introBG = document.getElementById('main-header')
+    let introSRC = [
+        "url('./assets/images/intro/intro_image_001.jpg')",
+        "url('./assets/images/intro/intro_image_002.jpg')",
+        "url('./assets/images/intro/intro_image_003.jpg')",
+        "url('./assets/images/intro/intro_image_004.jpg')"
+    ]
 
-    // const changeBG = () => {
-    //     introBG.style.backgroundImage = 
-    // }
+    let intervalID = 1;
+
+
+    // @ Fullscreen
+    const changeBG = () => {
+        switch (intervalID) {
+            case 1:
+                intervalID = 2;
+                introBG.style.backgroundImage = introSRC[1]
+                break;
+            case 2:
+                intervalID = 3;
+                introBG.style.backgroundImage = introSRC[2]
+                break;
+            case 3:
+                intervalID = 4;
+                introBG.style.backgroundImage = introSRC[3]
+                break;
+            case 4:
+                intervalID = 1;
+                introBG.style.backgroundImage = introSRC[0]
+                break;
+            default:
+                break;
+        }
+    }
+
+    setInterval(changeBG, 7000);
+
 
 })
