5381f3d516f242c8666b3a2b766e829ec1c6903b
David Rojo <rojo@rojorevolution.com>
2022-02-18 15:03:36 -0600

    set up basic files, working on intro section

---
+++ assets/js/intro-bg.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM loaded')
+
+    let introBG = document.getElementById('main-header')
+
+    // const changeBG = () => {
+    //     introBG.style.backgroundImage = 
+    // }
+
+})
