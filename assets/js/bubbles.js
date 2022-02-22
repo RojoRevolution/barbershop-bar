9474efec873dfba67521525ac48ad03796ffc66a
David Rojo <rojo@rojorevolution.com>
2022-02-21 21:38:55 -0600

    adjustments to animation opacity and count

--- assets/js/bubbles.js
+++ assets/js/bubbles.js
@@ -8,16 +8,11 @@ window.addEventListener('DOMContentLoaded', () => {
     let duration;
     let delay;
 
-
-
-    const getDuration = () => {
+    const randomize = () => {
         duration = Math.floor(1 + Math.random() * 12 + 1);
-        console.log('Duration: ', duration)
-    }
-
-    const getDelayAndType = () => {
         delay = Math.floor(Math.random() * 4);
         type = Math.floor(Math.random() * 4);
+        console.log('Duration: ', duration)
         console.log('Delay: ', delay);
         console.log('Type: ', type);
     }
@@ -27,9 +22,8 @@ window.addEventListener('DOMContentLoaded', () => {
         // getDelayAndType()
 
 
-        for (let i = 0; i < 72; i++) {
-            getDuration()
-            getDelayAndType()
+        for (let i = 0; i < 92; i++) {
+            randomize();
 
             let parentEl = document.createElement('div')
             parentEl.classList.add('bubble-container-single', 'pos-rel', 'd-flex')
