07f9963c96d530c82896af8f6f22d0bb07850377
David Rojo <rojo@rojorevolution.com>
2022-04-01 17:31:43 -0500

    added scroll change to main menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     window.addEventListener('scroll', () => {
 
         // console.log(window.scrollY)
-        if (window.scrollY >= 770) {
-            console.log('770')
+        if (window.scrollY <= 770) {
+            console.log('770');
+            menuBtn.classList.remove('menu-dark')
         }
-
+        if (window.scrollY >= 770) {
+            console.log('770');
+            menuBtn.classList.add('menu-dark')
+        }
     })
 
 });
